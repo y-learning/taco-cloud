a6e23aa2ee41a9f5b2fc75d5af327b65b77df806
Yahya Tinani <47183730+yahyatinani@users.noreply.github.com>
2023-11-08 20:06:33 +0100

    Initialize spring-web

---
+++ src/test/kotlin/io/github/yahyatinani/springbootstartermk/ApplicationTests.kt
@@ -0,0 +1,12 @@
+package io.github.yahyatinani.springbootstartermk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+  @Test
+  fun loadSpringApplicationContext() {
+    assert(1 + 1 == 2)
+  }
+}
